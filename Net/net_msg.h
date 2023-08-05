a45fcf2ff8af0711f3661be5ac82efe3d1035a46
prabinadkri <107361341+prabinadkri@users.noreply.github.com>
2023-08-05 19:00:12 +0545

    made clean

--- Net/net_msg.h
+++ Net/net_msg.h
@@ -70,17 +70,10 @@ namespace olc
 			
 			friend message<T>& operator << (message<T>& msg, std::vector<uint8_t> data)
 			{
-				// Check that the type of the data being pushed is trivially copyable
-				//static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
-
-				// Cache current size of vector, as this will be the point we insert the data
+		
 				//size_t i = msg.body.size();
 				msg.body = data;
-				// Resize the vector by the size of the data being pushed
-				//msg.body.resize(msg.body.size() + sizeof(DataType));
-
-				// Physically copy the data into the newly allocated vector space
-				//std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+				
 
 				// Recalculate the message size
 				msg.header.size = msg.size();
